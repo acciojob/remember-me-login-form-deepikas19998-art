b035f6974fa4db807e18df99adc859a99a999362
deepikas19998-art <deepikas19998@gmail.com>
2025-11-01 14:10:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener("click", function(e) {
+  e.preventDefault(); // prevent form submission
+  const username = document.getElementById("username").value;
+  const remember = document.getElementById("checkbox").checked;
+
+  alert(`Logged in as ${username}`);
+
+  if (remember) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", document.getElementById("password").value);
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+  }
+});
+
